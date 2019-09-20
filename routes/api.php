6f58fb68193aeae2b028507f59d907cb944e2f95
yangSir <asdf1995905@gmail.com>
2019-09-20 11:49:32 +0800

    取得Books

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('books', 'BookController@index');
+Route::get('books/{id}', 'BookController@show');
+Route::post('books', 'BookController@store');
+Route::put('books/{id}', 'BookController@update');
+Route::delete('books/{id}', 'BookController@destroy');
